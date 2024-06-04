39f4aa147df3e5bb9f7d16780834cec6266e43e3
李艺林02-移动端业务部 <liyilin02@megvii.com>
2024-06-04 09:38:00 +0800

    remove unnecessary code & add solutions call

--- main.cpp
+++ main.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
 #include <thread>
-#include "detect/DetectManager.h"
+
+#include "leetcode/Solutions.h"
+
+
+
 
 int main() {
-    DetectManager *m1 = new DetectManager();
-    DetectManager *m2 = new DetectManager();
-    DetectManager *m3 = new DetectManager();
-    m1->startDetect();
-    m2->startDetect();
-    m3->startDetect();
-    m1->getThread()->join();
-    m2->getThread()->join();
-    m3->getThread()->join();
-    return 0;
-}
-
-int copyData(int *src,int *dst,int size ){
-    int *copyData = new int[size];
-    memcpy(copyData,dst,size);
+    Solutions *solutions = new Solutions();
+    vector<int> *data = new vector<int>();
+    solutions->canPlaceFlowers(*data,1);
 }
 
 
