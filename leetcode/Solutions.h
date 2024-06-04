b0e4a2c87a38500d2ab16ad3361fe610da2a1be1
李艺林02-移动端业务部 <liyilin02@megvii.com>
2024-06-04 09:39:49 +0800

    add debug file to ignore list

--- leetcode/Solutions.h
+++ leetcode/Solutions.h
@@ -11,6 +11,7 @@
 
 using namespace std;
 class Solutions {
+public:
     /**
      * You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
      * Return the merged string.
